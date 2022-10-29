d372f89fc9d1d8670d36dfc2d832a5a50beba9e7
allexmorozovv <allexmorozovv@gmail.com>
2022-10-29 17:35:11 +0300

    add bubble sort

--- 05-sort/main.js
+++ 05-sort/main.js
@@ -50,5 +50,43 @@ console.log([...students.sort(ageSort)])
 const nameSort = (a, b) => a.name.localeCompare(b.name)
 console.log([...students.sort(nameSort)])
 
+// Bubble sort
+
+const nums1 = [54, 11, 36, 32, 68, 87, 23,]
+
+for (let j = 0; j < nums1.length - 1; j++) {
+
+    for (let i = 0; i < nums1.length - 1; i++) {
+        if (nums1[i] > nums1[i + 1]) {
+
+            const temp = nums1[i]
+            nums1[i] = nums1[i + 1]
+            nums1[i + 1] = temp
+        }
+    }
+}
+
+console.log(nums1)
+
+
+const nums2 = [54, 11, 36, 32, 68, 87, 23, 1, 22]
+
+for (let j = 0; j < nums2.length - 1; j++) {
+    let isSorted = true
+    for (let i = 0; i < nums2.length - 1 - j; i++) {
+        if (nums2[i] > nums2[i + 1]) {
+            isSorted = false;
+            //работает быстрее
+            // const temp = nums[i]
+            // nums[i] = nums[i + 1]
+            // nums[i + 1] = temp
+            [nums2[i + 1], nums2[i]] = [nums2[i], nums2[i + 1]]
+        }
+    }
+    if (isSorted) break
+}
+
+console.log(nums2)
+
 
 
